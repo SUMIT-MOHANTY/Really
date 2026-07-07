fe1d5bc77b8e1407deb515e3e6b9c1a0641c1068
Sumit Mohanty <sumitmohanty@Sumits-MacBook-Air.local>
2026-07-07 23:12:12 +0530

    fixed

--- 01_basics/start/ticket.py
+++ 01_basics/start/ticket.py
@@ -5,6 +5,7 @@ day = input("Please enter the day of the week: ")
 price = 12 if age <= 12 else 20
 
 if day == day2:
+    
         price = price-2
 
 print(f"Your ticket price is {price} dollars.")
