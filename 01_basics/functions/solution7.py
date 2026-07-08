071cb06213ab4fcb7e94658d893a00734137c10e
SUMIT MOHANTY <sumitmohanty2019@gmail.com>
2026-07-09 00:34:47 +0530

    added sencodn solution

---
+++ 01_basics/functions/solution7.py
@@ -0,0 +1,16 @@
+# lamda function
+
+# def add (x,y):
+#     return x + y
+
+# result = add(5, 10)
+# print(result)
+
+# this is basic 
+
+# in lamda function we can write the above code in one line
+
+cube = lambda x: x ** 3
+add = lambda x, y: x + y
+
+print(cube(5))
