23deccf6fe8a3f8b0db0f2167f877be3eb73246f
SUMIT MOHANTY <sumitmohanty2019@gmail.com>
2026-07-09 00:15:30 +0530

    added sencodn solution

---
+++ 01_basics/functions/soloution2.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number:"))
+num2 = int (input("Enter second number:"))
+
+def twonumbersum (num1,num2):
+    return num1 + num2
+
+result = twonumbersum(num1,num2)
+print(result) 
